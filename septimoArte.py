9a895e48855a2ac200c6d777e5e21edac08433ff
Jose Manuel Mendez <jmanuel34@gmail.com>
2019-02-22 22:45:04 +0100

    Testing

--- septimoArte.py
+++ septimoArte.py
@@ -1,13 +1,28 @@
-import re
-import urllib.request
-from pprint import pprint
+import requests
+import time
+from bs4 import BeautifulSoup
+URL = "https://www.ecartelera.com/peliculas/indice/o/"
+content = requests.get(URL)
+soup = BeautifulSoup(content.text, "html.parser")
+#results=[]
+items = soup.findAll('div',{'class': 'fl-item'})
+for item in items:
+    item["titulo"] = item.find('p', {'class': 'tit'}).find('a').get_text()
+    item["href"] = item.find('a').get('href')
+    print(item["titulo"])
+    print(item["href"])
+    URLdetail=item["href"]
+    contentDetail=requests.get(URLdetail)
+    soupDetail = BeautifulSoup(content.text,"html.parser")
+    itemsDetail= soup.findAll('table', {'class':'datospelicula'})
+    for itemDetail in itemsDetail.findAll('tr'):
+        detail = {}
+        columns=item.findAll('td')
+        detail["Anio"]= columns[0].find('td',{'class':'year'}).find('a').get_text()
+   #     itemDetail["Anio"] = item.find ('td',{'class':'year'}).find('a').get_text()
+        print(detail["Anio"])
+ #   time.sleep(3)
 
-#p = '/Users/jmanuel/Desktop/Ai/trafico/trafico1.txt'
-request = urllib.request.Request(
-    "http://infocar.dgt.es/etraffic/Incidencias?ca=13&provIci=28&caracter=acontecimiento&accion_consultar=Consultar&IncidenciasRETENCION=IncidenciasRETENCION&IncidenciasPUERTOS=IncidenciasPUERTOS&IncidenciasMETEOROLOGICA=IncidenciasMETEOROLOGICA&IncidenciasEVENTOS=IncidenciasEVENTOS&IncidenciasOTROS=IncidenciasOTROS&IncidenciasRESTRICCIONES=IncidenciasRESTRICCIONES&ordenacion=fechahora_ini-DESC")
-result = urllib.request.urlopen(request)
-pagina = (result.read().decode('utf8'))
-#patron1 = '<.*?>'
-#patronEspacio = '\s+'
-pprint (pagina)
 
+#columns[0].find('span', {'class': 'orange'}).get_text()
+#get('alt')
